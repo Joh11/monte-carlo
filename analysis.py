3758bb2205cda9c654e799b809fa3505f35357e7
Joh11 <johan74470@gmail.com>
2019-11-12 15:48:05 +0100

    choose the last Nsteps for mean

--- analysis.py
+++ analysis.py
@@ -3,9 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 filename = "data/data.out"
+Nlast = 10
 
 if len(sys.argv) >= 2:
     filename = sys.argv[1]
+
+if len(sys.argv) >= 3:
+    Nlast = sys.argv[1]
 
 data = np.loadtxt(filename)
 
@@ -14,6 +18,10 @@ E = data[:, 5]
 M = data[:, 6:9]
 
 Mz = M[:, 2]
+
+print("Using the last {} samples : ".format(Nlast))
+print("Mean energy per site : {} +- {}".format(np.mean(E[-Nlast:]), np.std(E[-Nlast:])))
+print("Mean magnetization per site : {}".format(np.mean(M[-Nlast:, :], axis=0)))
 
 # Plot energy against iteration
 plt.subplot(2, 1, 1)
